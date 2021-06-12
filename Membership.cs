114a7566df4cc1772d8d4f606aa74193237fa44c
SulekhaRoy <85783498+SulekhaRoy@users.noreply.github.com>
2021-06-12 14:30:05 +0530

    Add files via upload

---
+++ Membership.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessRules.Core.BusinessRules.BaseClass;
+using BusinessRules.Core.BusinessRules.Interface;
+
+namespace BusinessRules.Core
+{
+    public class Membership:IPayment
+    {
+        public string GetPayment(string option)
+        {
+            return "activate the membership";
+        }
+
+       
+    }
+}
